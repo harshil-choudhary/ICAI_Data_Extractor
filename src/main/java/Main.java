a30fd78b740cdcab092f2678ad725a76f8efdb41
Harshil Choudhary <harshil.choudhary@paytm.com>
2022-05-09 17:49:07 +0530

    Completable Futures and minor tweaks

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,45 +7,67 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.jsoup.nodes.Element;
 
+import javax.net.ssl.SSLSession;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
     static String url = "http://220.225.242.182/lom.asp";
+    static HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
 
-    private static String[] icaiSiteDetailsByMembershipNumber(Long membershipNumber) throws URISyntaxException, IOException, InterruptedException {
+    private static CompletableFuture<String[]> icaiSiteDetailsByMembershipNumber(Long membershipNumber) throws URISyntaxException, IOException, InterruptedException {
 
         // Building URL Param
-        String urlParameter = "t1=" + membershipNumber;
+        StringBuilder membershipNumberInString = new StringBuilder();
+        if (membershipNumber < 100000L) {
+            int numberOfPrecedingZeroes = 6 - String.valueOf(membershipNumber).length();
+            for (int i = 0; i<numberOfPrecedingZeroes; i++) {
+                membershipNumberInString.append("0");
+            }
+            membershipNumberInString.append(membershipNumber);
+        } else {
+            membershipNumberInString = new StringBuilder(Long.toString(membershipNumber));
+        }
+        String urlParameter = "t1=" + membershipNumberInString;
 
         int statusCode = 200;
         String responseBody = "";
         // HTTP Request
+        System.out.println("Getting Data for : " + membershipNumberInString);
         do {
-            HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(new URI(url))
                     .POST(HttpRequest.BodyPublishers.ofString(urlParameter))
                     .headers("Content-Type", "application/x-www-form-urlencoded")
                     .build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            responseBody = response.body();
-            statusCode = response.statusCode();
+            try {
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                responseBody = response.body();
+                statusCode = response.statusCode();
+            } catch (IOException e) {
+                System.out.println("IOException for " + membershipNumber);
+                statusCode = 500;
+            }
             if (statusCode >= 300) {
                 System.out.println("Retrying for " + membershipNumber + ". Status : " + statusCode);
             }
         } while (statusCode >= 300);
 
-        String membershipNumberString = Long.toString(membershipNumber);
         String name = "";
         String address = "";
 
@@ -70,8 +92,14 @@ public class Main {
                     + " "
                     + rows.get(9).select("td").get(1).text();
         }
-        System.out.println("Received Data : " + membershipNumberString + " " + name + " " + address);
-        return new String[]{membershipNumberString, name, address};
+
+        if (address.equals("") || name.equals("")) {
+            System.out.println("Received No Data for " + membershipNumberInString);
+        } else {
+            System.out.println("Received Data : " + membershipNumberInString + " " + name + " " + address);
+        }
+
+        return CompletableFuture.completedFuture(new String[]{String.valueOf(membershipNumberInString), name, address});
     }
 
     private static String convertLineToCSV(String[] data) {
@@ -79,30 +107,36 @@ public class Main {
     }
 
     private static void csvBuilder(Long membershipNumberStart, Long membershipNumberEnd) throws URISyntaxException, IOException, InterruptedException {
+        List<CompletableFuture<String[]>> allFutures = new ArrayList<>();
         // Compile data from each call
-        List<String[]> dataLines= new ArrayList<>();
-        dataLines.add(new String[]{"Membership No.", "Name", "Address"});
         for (Long i = membershipNumberStart; i<=membershipNumberEnd; i++) {
-            System.out.println("Getting Data for : " + i);
-            dataLines.add(icaiSiteDetailsByMembershipNumber(i));
+            allFutures.add(icaiSiteDetailsByMembershipNumber(i));
         }
+
+        CompletableFuture.allOf(allFutures.toArray(new CompletableFuture[0])).join();
+        String[] headers = new String[]{"Membership No.", "Name", "Address"};
 
         // Create CSV
         System.out.println("Compiling CSV...");
         File csvOutputFile = new File("ICAI_Data_" + membershipNumberStart + "_" +membershipNumberEnd + ".csv");
 
         try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
-            for (String[] dataLine : dataLines) {
-                String s = convertLineToCSV(dataLine);
+            pw.println(convertLineToCSV(headers));
+            for (int i = 0; i<allFutures.size(); i++) {
+                String s = convertLineToCSV(allFutures.get(i).get());
                 pw.println(s);
             }
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
     }
 
     public static void main(String args[]) throws URISyntaxException, IOException, InterruptedException {
-        Long membershipNumberStart = 100000L;
-        Long membershipNumberEnd = 999999L;
+        Long membershipNumberStart = 0L;
+        Long membershipNumberEnd = 99999L;
 
+        Instant start = Instant.now();
         csvBuilder(membershipNumberStart, membershipNumberEnd);
+        System.out.println("Done! Total time: " + Duration.between(start, Instant.now()).getSeconds() + " seconds");
     }
 }
